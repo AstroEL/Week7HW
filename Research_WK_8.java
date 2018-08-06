f3674031f48f37e86d9b5ae8c77169f0790a8733
Leiland <leiland12@gmail.com>
2018-08-06 13:21:10 -0700

    SQL WK 7 HW

---
+++ Research_WK_8.java
@@ -0,0 +1,36 @@
+
+public class Research_WK_8 {
+	/*
+	 * 	Research wild-cards in MySQL and write about why and how they are useful.
+	 * 
+	 * 	I'm going to rewrite the definition here to help me remember a little better - the two
+	 * 	used wild-cards in MySQL are the '%' and the '_'. 
+	 * 	These wild-characters can be used with the LIKE operator to substitute any other character
+	 * 	into a string. The % represents zero, one, or multiple characters ---- while the _ only
+	 * 	represents a single character. 
+	 * 
+	 * 	These wild-cards can be useful because it makes more specific searching and filtering 
+	 * 	possible where specific characters and/or character positions can be pinned and used
+	 * 	for changing, organizing or deleting data in the database.
+	 * 
+	 * 	https://www.w3schools.com/sql/sql_wildcards.asp
+	 *
+	 *	Research all the operators that can be used in a SQL WHERE clause. Write what they do.
+	 *
+	 *	In the SQL Where Clause, these operators help single out data by position in the 
+	 *	database for manipulation with boolean statements.
+	 *				= 		Equals
+	 *				<>		Not Equal (could be ! in some versions of SQL)
+	 *				>		Greater than
+	 *				<		Less than
+	 *				>=		Greater than or equal
+	 *				<=		Less than or equal
+	 *				BETWEEN	Identifies the values between an inclusive range
+	 *				LIKE	Search for a pattern - can use wild-cards (%_) 
+	 *				IN		To specify multiple possible values for a column -  I had
+	 *						to dig around on the web to find this out for the last question in the
+	 *						coding portion... I should really do the research portion first, 
+	 *						it would have saved some time. 
+	 *
+	 */
+}
